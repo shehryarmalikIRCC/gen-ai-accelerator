8d145e86fb342ef908f2130dbe134f808a8867e0
Sen Wang <senw@microsoft.com>
2024-09-26 13:52:48 -0400

    initial Angular project

---
+++ frontend/src/main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule)
+  ]
+})
+.catch(err => console.error(err));
